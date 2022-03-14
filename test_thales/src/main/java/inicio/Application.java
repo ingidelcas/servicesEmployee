749eb2c40230c9d17d564109506f712c8f2c7aeb
ingidelcas <98914551+ingidelcas@users.noreply.github.com>
2022-03-13 21:26:48 -0500

    configure generar .war

--- test_thales/src/main/java/inicio/Application.java
+++ test_thales/src/main/java/inicio/Application.java
@@ -2,6 +2,8 @@ package inicio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.client.support.BasicAuthenticationInterceptor;
@@ -9,7 +11,7 @@ import org.springframework.web.client.RestTemplate;
 
 @ComponentScan(basePackages = {"controller","services"})
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -19,5 +21,10 @@ public class Application {
 	public RestTemplate template(){
 		return new RestTemplate();
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
 
 }
